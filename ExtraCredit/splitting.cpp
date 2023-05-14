2a3c958d77cfffd5d85d233bc78f6070facd8179
Josh Lollis <lollisjosh@csu.fullerton.edu>
2023-05-14 13:43:36 -0700

    Added file read to splitting

--- ExtraCredit/splitting.cpp
+++ ExtraCredit/splitting.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
+
+
+bool readString(const std::string& filename, std::string& fillString)
+    {
+        // Open the file
+        std::ifstream file(filename);
+        if (!file) {
+            std::cerr << "Failed to open the file." << std::endl;
+            return false;
+        }
+
+        // Read the file contents into a string
+        fillString = std::string((std::istreambuf_iterator<char>(file)),
+                                std::istreambuf_iterator<char>());
+
+        // Close the file
+        file.close();
+        return true;
+    }
 
 std::vector<std::string> splitString(const std::string &str, int N)
 {
@@ -22,17 +42,23 @@ std::vector<std::string> splitString(const std::string &str, int N)
 
 int main()
 {
-    std::string input = "Hello, world!";
+    std::string filename{"input.txt"};
     int N = 7;
 
-    std::vector<std::string> substrings = splitString(input, N);
+    std::string inputString;
+    readString(filename, inputString);
+
+    std::vector<std::string> substrings = splitString(inputString, N);
 
     for (const auto &substring : substrings)
     {
         std::cout << substring << std::endl;
     }
 
-    if (N > input.length())
+    if (N > inputString.length())
         std::cout << "N > input.length()..." << std::endl;
     return 0;
 }
+
+
+
